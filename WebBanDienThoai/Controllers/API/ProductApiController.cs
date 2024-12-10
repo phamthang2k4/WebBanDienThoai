deb89bf2c34985195a5be573f8ec5e4914e225ba
phamthang2k4 <phamthang6636@gmail.com>
2024-12-10 15:56:35 +0700

    Add - ProductApi and Ajax for Menu

---
+++ WebBanDienThoai/Controllers/API/ProductApiController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QLBanSach.Models.ProductModels;
+using WebBanDienThoai.Models;
+
+namespace WebBanDienThoai.Controllers.API
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductApiController : Controller
+    {
+        BtlLtwQlbdtContext db = new BtlLtwQlbdtContext();
+        [HttpGet]
+        public IEnumerable<Product> GetAllProduct()
+        {
+            var sanPham = (from p in db.SanPhams
+                           select new Product
+                           {
+                               MaSanPham = p.MaSanPham,
+                               TenSanPham = p.TenSanPham,
+                               AnhDaiDien = p.AnhDaiDien,
+                               DonGiaBanRa = p.DonGiaBanRa,
+                               MaHang = p.MaHang,
+                           }).ToList();
+            return sanPham;
+        }
+        [HttpGet("{mahang}")]
+        public IEnumerable<Product> GetProductsByCategory(string mahang)
+        {
+            var sanPham = (from p in db.SanPhams
+                           where p.MaHang == mahang
+                           select new Product
+                           {
+                               MaSanPham = p.MaSanPham,
+                               TenSanPham = p.TenSanPham,
+                               AnhDaiDien = p.AnhDaiDien,
+                               DonGiaBanRa = p.DonGiaBanRa,
+                               MaHang = p.MaHang,
+                           }).ToList();
+            return sanPham;
+        }
+    }
+}
---
+++ WebBanDienThoai/Models/ProductModels/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace QLBanSach.Models.ProductModels
+{
+    public class Product
+    {
+        public string MaSanPham { get; set; } = null!;
+
+        public string? TenSanPham { get; set; }
+
+        public string? AnhDaiDien { get; set; }
+
+        public decimal? DonGiaBanRa { get; set; }
+
+        public string? MaHang { get; set; }
+    }
+}
