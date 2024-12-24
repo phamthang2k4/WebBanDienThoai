9d9b682b4fa4f88343461a14149182cf66fe865a
phamthang2k4 <phamthang6636@gmail.com>
2024-12-24 15:36:20 +0700

    Add - Admin Area

---
+++ WebBanDienThoai/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebBanDienThoai.Models;
+
+namespace WebBanDienThoai.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin")]
+    [Route("admin/homeadmin")]
+    public class HomeAdminController : Controller
+    {
+        BtlLtwQlbdtContext db = new BtlLtwQlbdtContext();
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
