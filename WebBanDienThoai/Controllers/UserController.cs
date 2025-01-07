e727347150471feb66f9b61e5b2c203f692037eb
phamthang2k4 <phamthang6636@gmail.com>
2025-01-07 16:44:21 +0700

    Add - Profile User

---
+++ WebBanDienThoai/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebBanDienThoai.Models;
+
+namespace WebBanDienThoai.Controllers
+{
+    public class UserController : Controller
+    {
+
+        private readonly BtlLtwQlbdtContext _context;
+
+        public UserController(BtlLtwQlbdtContext context)
+        {
+            _context = context;
+        }
+
+        // Action hiển thị trang Profile
+        [HttpGet]
+        public IActionResult Profile()
+        {
+            var model = new KhachHang
+            {
+                MaKhachHang = HttpContext.Session.GetString("MaKhachHang"),
+                TenKhachHang = HttpContext.Session.GetString("HoTen"),
+                NgaySinh = DateOnly.Parse(HttpContext.Session.GetString("NgaySinh") ?? DateOnly.MinValue.ToString()),
+                SoDienThoai = HttpContext.Session.GetString("SoDienThoai"),
+                DiaChi = HttpContext.Session.GetString("DiaChi"),
+                Email = HttpContext.Session.GetString("Email"),
+                GhiChu = HttpContext.Session.GetString("GhiChu"),
+                AnhDaiDien = HttpContext.Session.GetString("Avatar")
+            };
+            return View(model);
+        }
+    }
+}
