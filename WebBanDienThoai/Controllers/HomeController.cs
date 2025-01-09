37e4048ee12808a0a2445224bdaa44948ea68bc6
phamthang2k4 <phamthang6636@gmail.com>
2025-01-09 16:25:29 +0700

    Add - Count the number of product in cart

--- WebBanDienThoai/Controllers/HomeController.cs
+++ WebBanDienThoai/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using WebBanDienThoai.Models;
@@ -23,6 +23,23 @@ namespace WebBanDienThoai.Controllers
             var lstSanPham = db.SanPhams.AsNoTracking().OrderBy(x => x.MaHang);
             PagedList<SanPham> lst = new PagedList<SanPham>(lstSanPham, pageNumber, pageSize);
             return View(lst);
+        }
+
+        [HttpGet]
+        public int GetCartQuantity()
+        {
+            var userId = HttpContext.Session.GetString("Username");
+            if (string.IsNullOrEmpty(userId))
+                return 0;
+
+            var gioHang = db.GioHangs.FirstOrDefault(g => g.TenDangNhap == userId);
+            if (gioHang == null)
+                return 0;
+
+            // Tính tổng số lượng sản phẩm trong giỏ hàng
+            return db.ChiTietGioHangs
+                .Where(c => c.MaGioHang == gioHang.MaGioHang)
+                .Sum(c => c.SoLuong) ?? 0;
         }
 
         public IActionResult Privacy()
