79f2f758ffe77977092f370edd759407690e12ef
phamthangks <phamthang6636@gmail.com>
2025-02-12 21:55:55 +0700

    Add - Page Checkout and Shipping fee calculation function

--- WebBanDienThoai/Controllers/CheckoutController.cs
+++ WebBanDienThoai/Controllers/CheckoutController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebBanDienThoai.Models;
 
 namespace WebBanDienThoai.Controllers
 {
     public class CheckoutController : Controller
     {
+        private readonly BtlLtwQlbdtContext _context;
+
+        public CheckoutController(BtlLtwQlbdtContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            string userId = HttpContext.Session.GetString("Username");
+            if (string.IsNullOrEmpty(userId))
+            {
+                return RedirectToAction("Login", "Access");
+            }
+
+            var gioHang = _context.GioHangs.FirstOrDefault(g => g.TenDangNhap == userId);
+            if (gioHang == null)
+            {
+                return RedirectToAction("Index", "Cart");
+            }
+
+            var chiTietGioHang = _context.ChiTietGioHangs
+                .Include(c => c.MaGioHangNavigation)
+                .Include(c => c.MaSanPhamNavigation)
+                .Where(c => c.MaGioHang == gioHang.MaGioHang)
+                .ToList();
+            return View(chiTietGioHang);
         }
     }
 }
